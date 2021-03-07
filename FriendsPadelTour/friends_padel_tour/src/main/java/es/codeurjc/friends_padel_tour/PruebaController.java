1beca44a7f32a66870de75af0f23b6b0d03f6e85
eduvilla97 <eduvilla99@gmail.com>
2021-03-07 19:34:51 +0100

    Primera prueba de navegacion en la pantalla de inicio

--- FriendsPadelTour/friends_padel_tour/src/main/java/es/codeurjc/friends_padel_tour/PruebaController.java
+++ FriendsPadelTour/friends_padel_tour/src/main/java/es/codeurjc/friends_padel_tour/PruebaController.java
@@ -3,6 +3,8 @@ package es.codeurjc.friends_padel_tour;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -11,6 +13,11 @@ public class PruebaController {
     @GetMapping(value="/Index")
     public String getMethodName(Model model) {
         return  "Index";
+    }
+
+    @GetMapping(value="/team")
+    public String getMethodName2(Model model) {
+        return "team";
     }
     
     
