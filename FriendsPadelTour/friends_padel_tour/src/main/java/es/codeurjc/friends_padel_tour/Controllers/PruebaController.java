3154f51c359190984d983fcd0b740714f8bd3c5c
eduvilla97 <eduvilla99@gmail.com>
2021-03-08 11:23:51 +0100

    Creacion jerarquia proyecto

--- FriendsPadelTour/friends_padel_tour/src/main/java/es/codeurjc/friends_padel_tour/Controllers/PruebaController.java
+++ FriendsPadelTour/friends_padel_tour/src/main/java/es/codeurjc/friends_padel_tour/Controllers/PruebaController.java
@@ -1,9 +1,9 @@
-package es.codeurjc.friends_padel_tour;
+package es.codeurjc.friends_padel_tour.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
---
+++ FriendsPadelTour/friends_padel_tour/src/main/java/es/codeurjc/friends_padel_tour/Entities/Admin.java
@@ -0,0 +1,5 @@
+package es.codeurjc.friends_padel_tour.Entities;
+
+public class Admin extends User{
+    
+}
