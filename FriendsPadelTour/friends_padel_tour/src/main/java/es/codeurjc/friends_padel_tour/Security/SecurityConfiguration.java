33b9727cb72ef0f312c2cea914bfb55de6349576
JGBURJC <guillobermejo@gmail.com>
2021-03-19 21:51:05 +0100

    CAmbio en seguridad

--- FriendsPadelTour/friends_padel_tour/src/main/java/es/codeurjc/friends_padel_tour/Security/SecurityConfiguration.java
+++ FriendsPadelTour/friends_padel_tour/src/main/java/es/codeurjc/friends_padel_tour/Security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package es.codeurjc.friends_padel_tour.Security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,14 +11,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Value("${security.admin}")
+	private String admin;
+
+	@Value("${security.encodedPassword}")
+	private String encodedPassword;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
-		String encodedPassword = encoder.encode("pass");
-
-		auth.inMemoryAuthentication().withUser("admin").password(encodedPassword).roles("ADMIN");
+		auth.inMemoryAuthentication().withUser(admin).password("{bcrypt}" + encodedPassword).roles("ADMIN");
 	}
 
 	@Override
@@ -44,8 +49,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/tournaments").permitAll();
 		http.authorizeRequests().antMatchers("/userSignUp").permitAll();
 		http.authorizeRequests().antMatchers("/bussinessSignUp").permitAll();
+		http.authorizeRequests().antMatchers("/bussinessSignUp").permitAll();
         http.authorizeRequests().antMatchers("/css-min/**", "/css/main.css", "/css/**", "/js/**", "/images/**", "/fonts/**", "/dev-assets/**", "/vendor/**", "/html/**").permitAll();
 		
+
 		// Private pages (all other pages)
 		http.authorizeRequests().anyRequest().authenticated();
 
