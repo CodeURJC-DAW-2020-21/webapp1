ef1dccde3bbb20018f63d37e2797226ad738ec63
eduvilla97 <eduvilla99@gmail.com>
2021-04-12 12:02:47 +0200

    Create ApiMatchController.java

---
+++ FriendsPadelTour/friends_padel_tour/src/main/java/es/codeurjc/friends_padel_tour/Controllers/ApiMatchController.java
@@ -0,0 +1,10 @@
+package es.codeurjc.friends_padel_tour.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/matches")
+public class ApiMatchController {
+    
+}
