04045e23499d56bfc798b4ee0762130599d0d84e
eduvilla97 <eduvilla99@gmail.com>
2021-05-01 18:26:31 +0200

    inicio del login de usuarios

--- FriendsPadelTour/friends_padel_tour/frontend/src/app/login-form/login-form.component.ts
+++ FriendsPadelTour/friends_padel_tour/frontend/src/app/login-form/login-form.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../Service/login.service';
 
 @Component({
   selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  templateUrl: './login-form.component.html'
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  loginError : boolean = false;
+
+  constructor(public loginService: LoginService) { }
 
   ngOnInit(): void {
+  }
+
+  logIn(event : any, user : string, pass : string){
+    event.preventDefault();
+
+    this.loginService.logIn(user, pass);
   }
 
 }
